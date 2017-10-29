e9bd40e5699fb0d7270e5b0610a8422b9ab78b67
Ruben Silva <ruben.e.m.silva@hotmail.com>
2017-10-29 15:08:19 +0000

    Updated Database and added a few pages

---
+++ OnlineStore/OnlineStore/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineStore.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(int id)
+        {
+            return View();
+        }
+    }
+}
